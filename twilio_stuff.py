2ce44077979f71aa352e6f3e341f31caff3b87a6
Alison Rosenzweig <alisonhrosenzweig@gmail.com>
2016-12-03 23:33:46 +0000

    more twilio stuff

--- twilio_stuff.py
+++ twilio_stuff.py
@@ -1,5 +1,15 @@
 import twilio_private_stuff as tw
 import json
+import xmltodict
+
+# USER_NUMBER
+# FROM_NUMBER = 
+
+START_MSGS = ['Welcome to Health Texts! What should we ask you when we check in?',
+	      'What time during the day should we check in?',
+	      'Ok, text you later!']
+
+USERS = {}
 
 def get_messages_by_user_number(usr_phone_number_str):
     # get a list of all the sms messages
@@ -18,5 +28,9 @@ get_messages_by_user_number('+16152759927')
 
 def send_message(msg_str, usr_phone_number_str='+16152759927', from_number='+16156452860'):
     tw.client.messages.create(to=usr_phone_number_str, from_ = from_number, body=msg_str)
+
+
+def num_total_messages():
+    return len(tw.client.messages.list()['sms_messages'])
 
 send_message('test')
